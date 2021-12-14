08fa5804acbc1aaae85e6177c85c46be94cb65b0
Michael Scherer <michael.scherer@sealsystems.de>
2021-12-14 19:04:12 +0100

    Update build configuration

--- test/externalIp.js
+++ test/externalIp.js
@@ -5,7 +5,7 @@ const os = require('os');
 const flatten = require('lodash/flatten');
 const values = require('lodash/values');
 
-const externalIp = function() {
+const externalIp = function () {
   const nics = os.networkInterfaces();
 
   return flatten(values(nics)).find((address) => address.family === 'IPv4' && !address.internal).address;
